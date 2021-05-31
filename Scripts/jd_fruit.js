bf1908d536ed59edf7522eec7d83f3d17784d365
KMJ-2020 <67595019+KMJ-2020@users.noreply.github.com>
2021-05-31 14:01:26 +0800

    update

--- Scripts/jd_fruit.js
+++ Scripts/jd_fruit.js
@@ -30,7 +30,7 @@ let cookiesArr = [], cookie = '', jdFruitShareArr = [], isBox = false, notify, n
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [ // 这个列表填入你要助力的好友的shareCode
    //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
-  '3cc53af529e54da4914feea47a3f895a@0920b19b731f49ecb3098dafea2e7891',
+  '3cc53af529e54da4914feea47a3f895a@0920b19b731f49ecb3098dafea2e7891@d7a6345416c94ee79f955b1b03b80988',
   //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
   '3cc53af529e54da4914feea47a3f895a@0920b19b731f49ecb3098dafea2e7891',
 ]
